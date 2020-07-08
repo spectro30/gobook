42f301d49d807e399884abb0f343787cf7fe0dfe
spectro30 <alif@appscode.com>
2020-07-08 17:02:02 +0600

    ch12 added

---
+++ ch12/testCode.go
@@ -0,0 +1,35 @@
+// Actual implementation on ch11 folder
+package math
+
+import "testing"
+
+type testpair struct{
+	values []float64
+	average float64
+}
+
+var tests = []testpair{
+	{[]float64{1, 2}, 1.5},
+	{[]float64{1, 1, 1, 1, 1}, 1},
+	{[]float64{1, -1}, 1.5},
+}
+
+func TestAverage(t *testing.T){
+	for _, pair:= range tests{
+		v := Average(pair.values)
+		if v != pair.average{
+			t.Error(
+				"For", pair.values, "expected",
+				pair.average, "got", v,
+			)
+		}
+	}
+	var v float64
+	v = Average([]float64{1, 2})
+	if v != 1.5 {
+		t.Error("Expected 1.5, got ", v)
+	}
+}
+
+
+
